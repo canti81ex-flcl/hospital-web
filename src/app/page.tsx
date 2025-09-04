d98963834e90ae487846ef004e5d3ae71077f1d6
canti81ex-flcl <canti81ex@gmail.com>
2025-09-04 13:29:44 +0900

    fix: 빌드 타입 에러 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import home from "@/content/pages/home.json";
-import { RenderBlocks } from "@/lib/blocks";
+import { RenderBlocks, type Block } from "@/lib/blocks";
 import FullPageWrapper from "@/components/FullPageWrapper";
 
 export default function Page() {
   return (
     <FullPageWrapper>
-      <RenderBlocks blocks={home.blocks} />
+      <RenderBlocks blocks={home.blocks as Block[]} />
     </FullPageWrapper>
   );
 }
